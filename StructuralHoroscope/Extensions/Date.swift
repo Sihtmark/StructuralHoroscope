c1a722ffe724fc829504124031e1ccc25a0fe9c9
Sergei Poluboiarinov <sihtmark@icloud.com>
2023-05-15 12:48:41 +0300

    replace two files

--- StructuralHoroscope/Extensions/Date.swift
+++ StructuralHoroscope/Extensions/Date.swift
@@ -6,34 +6,35 @@
 //
 
 import Foundation
+import SwiftUI
 
 extension Date {
     private var shortFormatter: DateFormatter {
         let formatter = DateFormatter()
-        formatter.dateStyle = .short
+        formatter.dateStyle = .medium
         return formatter
     }
-    func asShortDateString() -> String {
-        return shortFormatter.string(from: self)
-    }
-    func diff(numDays: Int) -> Date {
-        Calendar.current.date(byAdding: .day, value: numDays, to: self)!
-    }
+//    func asShortDateString() -> String {
+//        return shortFormatter.string(from: self)
+//    }
+//    func diff(numDays: Int) -> Date {
+//        Calendar.current.date(byAdding: .day, value: numDays, to: self)!
+//    }
     var startOfDay: Date {
         Calendar.current.startOfDay(for: self)
     }
-    func noon(using calendar: Calendar = .current) -> Date {
-        calendar.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
-    }
-    func day(using calendar: Calendar = .current) -> Int {
-        calendar.component(.day, from: self)
-    }
+//    func noon(using calendar: Calendar = .current) -> Date {
+//        calendar.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
+//    }
+//    func day(using calendar: Calendar = .current) -> Int {
+//        calendar.component(.day, from: self)
+//    }
     func adding(_ component: Calendar.Component, value: Int, using calendar: Calendar = .current) -> Date {
         calendar.date(byAdding: component, value: value, to: self)!
     }
-    func monthSymbol(using calendar: Calendar = .current) -> String {
-        calendar.monthSymbols[calendar.component(.month, from: self)-1]
-    }
+//    func monthSymbol(using calendar: Calendar = .current) -> String {
+//        calendar.monthSymbols[calendar.component(.month, from: self)-1]
+//    }
     func addDate() -> Date {
         let calendar = Calendar.current
         return calendar.date(byAdding: .day, value: 12, to: self)!
